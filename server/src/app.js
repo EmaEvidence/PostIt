8232cfa536838175a4c41e810a5075e697cc1fcf
EmaEvidence <emmanuelalabi563@gmail.com>
2017-06-16 20:32:28 +0100

    Fix SignUp and LogIn error

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import validator from 'express-validator';
 import User from './post';
+
 
 const user = new User();
 const Router = express.Router();
 Router.use(bodyParser.json());
 Router.use(bodyParser.urlencoded({ extended: true }));
+Router.use(validator());
 
 
 Router.post('/api/user/signup', (req, res) => {
@@ -13,16 +16,33 @@ Router.post('/api/user/signup', (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
-  //console.log(name);
-  const me = user.signUp(name, username, email, password);
-  console.log(me);
-  res.send(me);
+  user.signUp(name, username, email, password, (result) => {
+    console.log(result);
+    res.send(result);
+  });
 });
 
 Router.post('/api/user/signin', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  const me = user.LogIn(username, password);
+  if (username === undefined || username === '') {
+    res.send('Username can not be empty');
+  } else if (password === undefined || password === '') {
+    res.send('Password can not be empty');
+  } else {
+    user.logIn(username, password, (result) => {
+      res.send(result);
+    });
+  }
+});
+
+Router.post('/api/group', (req, res) => {
+  const me = user.createGroup();
+  res.send(me);
+});
+
+Router.post('/api/group/group id/user', (req, res) => {
+  const me = user.createGroup();
   res.send(me);
 });
 
--- server/src/post.js
+++ server/src/post.js
@@ -7,7 +7,6 @@ import Promise from 'bluebird';
  */
 class User {
   constructor() {
-    this.Loginres = '';
     const username = 'ldgtnhia';
     const password = 'eD38PggvdWn9EVRdZi12DuhwrfECTqo8@pelle';
     const host = 'elephantsql.com:5432';
@@ -28,107 +27,110 @@ class User {
     this.Users = Users;
 
     const Groups = this.sequelize.define('Groups', {
-      name: { type: Sequelize.STRING, allowNull: false, unique: true },
-      userId: { type: Sequelize.INTEGER, allowNull: false }
+      gp_name: { type: Sequelize.STRING, allowNull: false, unique: true },
     });
     this.Groups = Groups;
-
+    Users.hasOne(Groups, { as: 'gp_creatorId' });
     const GroupMembers = this.sequelize.define('GroupMembers', {
-      groupId: { type: Sequelize.INTEGER, allowNull: false },
-      userId: { type: Sequelize.INTEGER, allowNull: false }
+      addedBy: { type: Sequelize.INTEGER, allowNull: false }
     });
     this.GroupMembers = GroupMembers;
+    Users.belongsToMany(Groups, { through: 'GroupMembers' });
 
     const Messages = this.sequelize.define('Messages', {
-      groupId: { type: Sequelize.INTEGER, allowNull: false },
-      userId: { type: Sequelize.INTEGER, allowNull: false },
       message: { type: Sequelize.TEXT, allowNull: false },
       priority: { type: Sequelize.STRING, allowNull: false }
     });
     this.Messages = Messages;
-  }
-  static respond(data){
-    console.log('12345');
-    return data;
-  }
-  signUp(userName, userUsername, userEmail, userPassword) {
-    const saltRounds = 10;
-    bcrypt.hash(userPassword, saltRounds, (err, hash) => {
-      this.sequelize.sync({}).then(() => {
-        this.Users.create({
-          name: userName,
-          username: userUsername,
-          email: userEmail,
-          password: hash
-        }).then(user => {
-          //console.log('Done');
-          this.Loginres = user;
-          User.respond(user);
-          return 'Done';
-        }).catch((err) => {
-        //console.log(err);
-          return err;
-        });
-        return 'We are good';
-      });
-    });
-    return this.Loginres;
+    Messages.belongsTo(Groups, { as: 'group_Id' });
+    Messages.belongsTo(Users, { as: 'sender_Id' });
+    this.sequelize.sync({ force: true });
   }
 
-  LogIn(userName, password) {
-    this.sequelize.sync({}).then(() => {
-      this.Users.findAll({
-        where: {
-          username: userName
-        }
+  signUp(userName, userUsername, userEmail, userPassword, done) {
+    const saltRounds = 10;
+    bcrypt.hash(userPassword, saltRounds, (err, hash) => {
+      this.Users.create({
+        name: userName,
+        username: userUsername,
+        email: userEmail,
+        password: hash
       }).then((user) => {
-        if (user.length === 0) {
-          console.log('Failed');
-          return 'Failed, Username not Found';
-        } else {
-          console.log(user[0].password);
-          bcrypt.compare(password, user[0].password, (err, res) => {
-            if (res) {
-              console.log('successfully');
-              return user;
-            } else {
-              console.log('Wrong password');
-              return 'Failed, Wrong Password';
-            }
-          });
-        }
+        done(user);
       }).catch((err) => {
-        console.log(err);
+        done(err.errors[0].message);
       });
     });
+  }
+
+  logIn(userName, password, done) {
+    this.Users.findAll({
+      where: {
+        username: userName
+      }
+    }).then((user) => {
+      if (user.length === 0) {
+        done('Failed, Username not Found');
+      } else {
+        console.log(user[0].password);
+        bcrypt.compare(password, user[0].password, (err, res) => {
+          if (res) {
+            console.log('successfully');
+            done(user);
+          } else {
+            console.log('Wrong password');
+            done('Failed, Wrong Password');
+          }
+        });
+      }
+    })
   }
 
   createGroup(groupName, creator) {
-    this.sequelize.sync({}).then(() => {
-      this.Groups.create({
-        name: groupName,
-        userId: creator
-      }).then(group => {
-        console.log(group);
-        return 'Done';
-      }).catch((err) => {
-      //console.log(err);
-        return err;
-      });
+    this.Groups.create({
+      gp_name: groupName,
+      gp_creatorId: creator
+    }).then((group) => {
+      console.log(group);
+      return 'Done';
+    }).catch((err) => {
+      console.log(err);
+      return err;
     });
   }
 
-  static addUsers() {
-    /* when the add button is clicked the id of the user will b
-     * e saved into an array clicking done will then loop through it and
-    //save it into the db.
-     if created the ouser */
+  addUsers(group, idOfUserAdding, idOfUserToAdd) {
+// will create a oject mapped with memberId the value will be either successfully or Failed
+// this will e the returned value
+    this.Groups.create({
+      groupId: group,
+      memberId: idOfUserToAdd,
+      addedBy: idOfUserAdding
+    }).then(member => {
+      console.log(member);
+      return 'Done';
+    }).catch((err) => {
+      console.log(err);
+      return err;
+    });
   }
 
-  static PostMessage() {
+  static PostMessage(to, from, text, priorityLevel) {
+    this.Groups.create({
+      groupId: to,
+      userId: from,
+      message: text,
+      priority: priorityLevel
+    }).then((message) => {
+      console.log(message);
+      return 'Done';
+    }).catch((err) => {
+      console.log(err);
+      return err;
+    });
   }
 
-  static RetrieveMessage() {
+  static RetrieveMessage(group, userId) {
 
   }
 }
