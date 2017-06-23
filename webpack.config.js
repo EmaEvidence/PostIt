022af7dd695cfab8db6b1a5b7291705fea6ee397
EmaEvidence <emmanuelalabi563@gmail.com>
2017-06-23 13:02:07 +0100

    Add sass transpiling to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: ['./client/index.js'],
+  entry: ['./client/index.js', './client/scss/main.scss'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'client/dist')
   },
   module: {
     rules: [
-     { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+     { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1',
+        }),
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'client/dist/[name].bundle.css',
+      allChunks: true,
+    }),
+  ],
 };
