53c20ae8e1fe4b58d20314c856f7deb856016ff3
EmaEvidence <emmanuelalabi563@gmail.com>
2017-07-15 21:27:20 +0100

    add form to add new members to a group

--- template/js/creategroup.js
+++ template/js/creategroup.js
@@ -15,4 +15,20 @@ $(document).ready(function() {
     minLength: 1
   }
   });
+  $('.chips-autocompletegroup').material_chip({
+    placeholder: 'Enter a group name',
+    secondaryPlaceholder: 'e.g. Cohort 1',
+    autocompleteOptions: {
+    data: {
+      'Cohort 1': null,
+      'Family': null,
+      'Developers': null,
+      'Campers': null,
+      'TTLs': null,
+      'STCs': null
+    },
+    limit: Infinity,
+    minLength: 1
+  }
+  });
 });
