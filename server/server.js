2205962271ff6205598f31803ef0dff9c008784b
EmaEvidence <emmanuelalabi563@gmail.com>
2017-07-26 20:08:12 +0100

    fix api rejection error

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(session({ secret: 'ssshhhhh' }));
 app.use('/', Router);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log('We are live');
 });
