ef8c140162ebc1b9439afee9da0e532d5d6d2dd4
EmaEvidence <emmanuelalabi563@gmail.com>
2017-08-15 12:14:37 +0100

    fix travis error

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import session from 'express-session';
 import Router from './src/route';
 
 const app = express();
@@ -13,9 +12,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
-app.use(session({ secret: 'ssshhhhh' }));
-app.use('/', Router);
-
 
 const server = app.listen(process.env.PORT || 3300, () => {
   console.log('We are live');
