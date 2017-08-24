f11187e7bbe82c553f3e98b537906607c700622e
EmaEvidence <emmanuelalabi563@gmail.com>
2017-08-24 18:23:13 +0100

    feat(server):add version to routes
    add version to api
    
    [Delivers]

--- server/src/route.js
+++ server/src/route.js
@@ -13,79 +13,79 @@ Router.get('/', (req, res) => {
   res.status(200).send('Post It API running');
 });
 
-Router.get('/api/user/all', controler.getAllUsersControler);
+Router.get('/api/v1/user/all', controler.getAllUsersControler);
 /**
  * signup router for registring new user
  *
  * @param  {form data} typeof result !== 'object' User data
  * @return {JSON}                            User object
  */
-Router.post('/api/user/signup', controler.signupControler);
+Router.post('/api/v1/user/signup', controler.signupControler);
 
 /**
  * [Sign in router for authenticating a user]
  * @type {[type]}
  */
-Router.post('/api/user/signin', controler.signinControler);
+Router.post('/api/v1/user/signin', controler.signinControler);
 
 /**
  * Group router for creating a new group
  * @type {[JSON]}
  */
-Router.post('/api/group', controler.ensureToken, controler.createGroupControler);
+Router.post('/api/v1/group', controler.ensureToken, controler.createGroupControler);
 
 /**
  * For adding a User to a created group
  * @type {[JSON]}
  */
-Router.post('/api/group/:groupid/user', controler.ensureToken, controler.addUserControler);
+Router.post('/api/v1/group/:groupid/user', controler.ensureToken, controler.addUserControler);
 
 /**
  * For getting every member of an existing group
  * @type {[JSON]}
  */
-Router.get('/api/group/:groupid/users', controler.ensureToken, controler.getGroupUsersControler);
+Router.get('/api/v1/group/:groupid/users', controler.ensureToken, controler.getGroupUsersControler);
 
 /**
  * For Posting messages to a group.
  * @type {[JSON]}
  */
-Router.post('/api/group/:groupid/message', controler.ensureToken, controler.postMessageControler);
+Router.post('/api/v1/group/:groupid/message', controler.ensureToken, controler.postMessageControler);
 
 /**
  * For getting messages posted to a group
  * @type {[type]}
  */
-Router.get('/api/group/:groupid/messages', controler.ensureToken, controler.getGroupMessagesControler);
+Router.get('/api/v1/group/:groupid/messages', controler.ensureToken, controler.getGroupMessagesControler);
 
 /**
  * [For deleting Users]
  * @type {[JSON]}
  */
 // reewrite this api like api/user/delete
-Router.post('/api/delete', controler.ensureToken, controler.deleteUserControler);
+Router.post('/api/v1/delete', controler.ensureToken, controler.deleteUserControler);
 
 /**
  * [For Groups a Users belongs to]
  * @type {[JSON]}
  */
-Router.get('/api/user/groups', controler.ensureToken, controler.getUserGroupsControler);
+Router.get('/api/v1/user/groups', controler.ensureToken, controler.getUserGroupsControler);
 
 // New routes
 
-Router.post('/api/user/message/read', controler.ensureToken, controler.messageReadControler);
+Router.post('/api/v1/user/message/read', controler.ensureToken, controler.messageReadControler);
 
-Router.post('/api/users/search', controler.ensureToken, controler.searchUserControler);
+Router.post('/api/v1/users/search', controler.ensureToken, controler.searchUserControler);
 
 // Router.post('/api/user/google', controler.googleSignUpControler);
 
-Router.post('/api/user/forgetpassword', controler.forgetPasswordControler);
+Router.post('/api/v1/user/forgetpassword', controler.forgetPasswordControler);
 
-Router.post('/api/user/newpassword', controler.newPasswordControler);
+Router.post('/api/v1/user/newpassword', controler.newPasswordControler);
 
-Router.get('/api/user/mymessage', controler.ensureToken, controler.mymessageControler);
+Router.get('/api/v1/user/mymessage', controler.ensureToken, controler.mymessageControler);
 
-Router.get('/api/user/archivedMessages', controler.ensureToken, controler.archivedMessagesControler);
+Router.get('/api/v1/user/archivedMessages', controler.ensureToken, controler.archivedMessagesControler);
 
 
 Router.get('*', (req, res) => {
